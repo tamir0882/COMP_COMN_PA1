b1889940db2d207ae943c4bd5c177adda8fa1ee9
tamir0882 <58632401+tamir0882@users.noreply.github.com>
2021-03-20 10:45:02 +0200

    Add files via upload

--- communication.c
+++ communication.c
@@ -23,7 +23,7 @@
 int send_data(const char* str, SOCKET communication_socket, SOCKADDR_IN recv_addr)
 {
 	/* Send the the request to the server on socket sd */
-	int total_string_size = strlen(str);
+	int total_string_size = strlen(str) + 1;
 
 	return send_buffer(str, total_string_size, communication_socket, recv_addr);
 }
@@ -58,22 +58,22 @@ int send_buffer(const char* buffer, int bytes_to_send, SOCKET s_send, SOCKADDR_I
 }
 
 
-int recv_data(char* buffer_recv, SOCKET s_recv, SOCKADDR_IN sender_addr)
+int recv_data(char* buffer_recv, int buf_size, SOCKET s_recv, SOCKADDR_IN sender_addr)
 {
 	if (NULL == buffer_recv)
 	{
 		return FAILURE;
 	}
 
-	int ret_val = 0;
+	int recv_bytes = 0;
 	int recv_res = SUCCESS;
 	
 	int send_addr_size = sizeof(sender_addr);
 
 	printf("inside recv_data, attempting recvfrom.\n");
 
-	ret_val = recvfrom(s_recv, buffer_recv, sizeof(buffer_recv), 0, (SOCKADDR*)&sender_addr, &send_addr_size);
-	if (ret_val == SOCKET_ERROR) 
+	recv_bytes = recvfrom(s_recv, buffer_recv, buf_size, 0, (SOCKADDR*)&sender_addr, &send_addr_size);
+	if (recv_bytes == SOCKET_ERROR) 
 	{
 		printf("recvfrom failed with error %d\n", WSAGetLastError());
 		return FAILURE;
